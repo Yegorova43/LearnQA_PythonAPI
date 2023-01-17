f0f56a21ea518f577dfe0d0cedc015b5333f400b
ma.egorova1 <ma.egorova1@tensor.ru>
2023-01-17 22:14:54 +0300

    Доработано ДЗ7

--- Ex7.py
+++ Ex7.py
@@ -3,13 +3,10 @@ import requests
 response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type")
 #2
 response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type")
-
 #3
 response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data = 'post')
 
 #4
-
-
                                       #проверяем метод GET
 method_lst = ['get', 'post', 'put', 'delete']
 
