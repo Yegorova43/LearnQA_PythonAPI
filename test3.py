9bc8788a7f0fa0184aa604eb570ab72dafdbaecb
ma.egorova1 <ma.egorova1@tensor.ru>
2023-01-22 18:33:41 +0300

    Добавлено ДЗ10

--- test3.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import builtins
-phrase = input("Set a phrase: ")
-
-def test_check_phrase(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "Mark")
-    assert phrase == "Mark", f"Wrong phrase length"
-
-test_check_phrase(phrase)
