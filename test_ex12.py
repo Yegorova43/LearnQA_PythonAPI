a72320c93fc83749131defde408f3d8472192076
ma.egorova1 <ma.egorova1@tensor.ru>
2023-01-26 19:56:51 +0300

    Исправлено ДЗ12

--- test_ex12.py
+++ test_ex12.py
@@ -3,9 +3,9 @@ class TestHeaders:
     def test_check_headers(self):
         url = "https://playground.learnqa.ru/api/homework_header"
         response = requests.get(url)
-        print(response.text)
+        print(response.headers)
 
 
         assert response.status_code == 200, "Wrong response code"
-        assert 'success' in response.text, 'There is no success in response'
+        assert 'x-secret-homework-header' in response.headers, 'There is no x-secret-homework-header in response'
 
